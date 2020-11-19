dba8e9701c46427949cb7aca8bf2c48d90dbf47e
Khongorzul <khongorzulodonkhuu@gmail.com>
2020-11-19 15:24:35 +0800

    Create Day-0.js

---
+++ Day-0.js
@@ -0,0 +1,10 @@
+
+
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    //add line
+    console.log(parameterVariable);
+    
+    
+}
