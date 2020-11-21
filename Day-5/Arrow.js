c2ab179d77a7a0726a9caf656feb42ed6b189280
Your Name <zul@Khongorzuls-MacBook-Pro.local>
2020-11-21 16:01:26 +0800

    day5

---
+++ Day-5/Arrow.js
@@ -0,0 +1,10 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+function modifyArray(nums) {
+    var arr = nums.map(n => n = (n%2==0) ? n*2: n*3);
+    return arr;
+}
